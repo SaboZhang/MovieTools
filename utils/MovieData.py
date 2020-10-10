ce2e4f09f70c935bd94d471055ef9ec0aa15c174
Sugar <tao993859833@live.cn>
2020-10-10 18:39:38 +0800

    本地文件夹中视频文件信息读取工具

---
+++ utils/MovieData.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+#----------------------------------------
+# @Author: 张涛
+# @Date: 2020-10-10 18:36:56
+# @LastEditTime: 2020-10-10 18:38:11
+# @LastEditors: 张涛
+# @Description: 读取本地视频文件夹中视频信息
+# @FilePath: \utils\MovieData.py
+#----------------------------------------
+import os
