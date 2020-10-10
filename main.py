7b7226e2c8fc3b45498fd6c4e9cc5e2104e53640
Sugar <tao993859833@live.cn>
2020-10-10 18:34:56 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#
+ # @Author: 张涛
+ # @Date: 2020-10-09 17:22:07
+ # @LastEditTime: 2020-10-10 12:56:20
+ # @LastEditors: 张涛
+ # @Description: 主程序、入口程序
+ # @FilePath: \main.py
+ # @Copyright © 2020 Person. All rights reserved.
+ #
+import multiprocessing
+import sys
+import os
+import PyQt5
